295dde8348e26663a6215c167608d8cae05d2d98
Maksymilian <99613189+maksik997@users.noreply.github.com>
2024-06-11 15:37:27 +0200

    # Changes - 2
    -- File Operator seems ready
    -- logging interface have 2 new methods.
    -- File Operator seems efficient (i don't think it's possible (for now) for me to create something better).

--- src/main/java/pl/magzik/IO/FileOperator.java
+++ src/main/java/pl/magzik/IO/FileOperator.java
@@ -6,20 +6,15 @@ import pl.magzik.Utils.LoggingInterface;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.FileSystemException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.UncheckedIOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.*;
 import java.util.stream.Stream;
 
 public class FileOperator implements LoggingInterface {
     private Path destination;
-
-    private final List<File> output;
 
     private ExecutorService virtualExecutor = Executors.newVirtualThreadPerTaskExecutor();
 
@@ -30,7 +25,6 @@ public class FileOperator implements LoggingInterface {
     public FileOperator(Path destination) {
         log("Created FileOperator.");
         this.destination = destination;
-        this.output = Collections.synchronizedList(new LinkedList<>());
     }
 
     public Path getDestination() {
@@ -50,99 +44,104 @@ public class FileOperator implements LoggingInterface {
         log("Loading files from " + source);
 
         log("Validating files");
-        if (destination == null) {
-            RuntimeException re = new NullPointerException("Destination is null.");
-            log(re, "Error: ");
-            throw re;
-        }
-
-        if (source == null) {
-            RuntimeException re = new IllegalArgumentException("Source is null.");
-            log(re, "Error:");
-            throw re;
-        }
+        Objects.requireNonNull(destination, "Destination is null");
+        Objects.requireNonNull(source, "Depth is null");
 
         try {
             source.parallelStream()
-                    .filter(f -> !f.exists())
-                    .findAny()
-                    .ifPresent(f -> {
-                        throw new RuntimeException("Could not find file " + f);
-                    });
+                .filter(f -> !f.exists())
+                .findAny()
+                .ifPresent(f -> {
+                    throw new RuntimeException("Could not find file " + f);
+                });
         } catch (RuntimeException e) {
             throw new FileNotFoundException(e.getMessage());
         }
 
         log("Files validated.");
 
-        //List<File> output;
+        List<File> output;
 
         log("Collecting files from " + source);
         try {
-            output.addAll(source.parallelStream()
-                    .filter(File::isFile)
-                    .filter(f -> {
-                        try {
-                            return fp.test(f);
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    })
-                    .toList());
-        } catch (RuntimeException ex) {
-            throw new IOException(ex.getMessage());
+            output = new LinkedList<>(source.parallelStream()
+                .filter(File::isFile)
+                .filter(f -> {
+                    try {
+                        return fp.test(f);
+                    } catch (IOException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                })
+                .toList());
+        } catch (UncheckedIOException ex) {
+            throw ex.getCause();
         }
 
         log("Directory files collection.");
 
-        try {
-            source.parallelStream()
-                    .filter(File::isDirectory)
-                    .map(File::toPath)
-                    .forEach(p -> {
-                        try (Stream<Path> s = Files.walk(p, depth)) {
-                            s.filter(Files::isRegularFile)
-                                    .forEach(path -> virtualExecutor.submit(() -> {
-                                        try {
-                                            processPath(fp, path, output);
-                                        } catch (IOException e) {
-                                            // tmp
-                                            if (e instanceof FileSystemException) log("Skipping " + path + " because " + e.getMessage());
-                                            else throw new RuntimeException("Could not process file " + p, e);
-                                        }
-                                    }));
-                        } catch (IOException e) {
-                            throw new RuntimeException("Could not walk " + p, e);
-                        }
-                    });
-        } catch (RuntimeException ex) {
-            // tmp
-            if (ex.getMessage().contains("FileSystemException")) log("Skipping because " + ex.getMessage());
-            else throw new IOException(ex.getMessage());
-        }
+        List<Future<File>> filesTasks = new LinkedList<>();
+
+        source.stream()
+                .filter(File::isDirectory)
+                .map(File::toPath)
+                .forEach(p -> {
+                    try {
+                        Files.walkFileTree(p, EnumSet.noneOf(FileVisitOption.class), depth, new SimpleFileVisitor<>() {
+                            @Override
+                            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                                if (Files.isRegularFile(file)) {
+                                    filesTasks.add(virtualExecutor.submit(() -> processPath(fp, file, output)));
+                                }
+
+                                return FileVisitResult.CONTINUE;
+                            }
+
+                            @Override
+                            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                                log("Skipping " + file + " because " + exc);
+                                return FileVisitResult.CONTINUE;
+                            }
+                        });
+                    } catch (IOException e) {
+                        throw new UncheckedIOException("Could not walk " + p, e);
+                    }
+                });
 
         virtualExecutor.shutdown();
-        long timeout = 1;
-        while (!virtualExecutor.awaitTermination(timeout, TimeUnit.MINUTES)) {
-            if (timeout > 10) {
-                virtualExecutor.shutdownNow();
-                if (virtualExecutor.awaitTermination(1, TimeUnit.MINUTES)) {
-                    Thread.currentThread().interrupt();
+        // For now time for file searching is 10 minutes.
+        long timeout = 600;
+        if (!virtualExecutor.awaitTermination(timeout, TimeUnit.SECONDS)) {
+            virtualExecutor.shutdownNow();
+            if (!virtualExecutor.awaitTermination(30, TimeUnit.SECONDS)) {
+                throw new TimeoutException("Executor couldn't end created threads.");
+            }
+        }
+        
+        virtualExecutor.close();
+        virtualExecutor = Executors.newVirtualThreadPerTaskExecutor();
+
+        try {
+            for (Future<File> future : filesTasks) {
+                try {
+                    File f = future.get();
+                    if (f != null) output.add(f);
+                } catch (ExecutionException e) {
+                    log(e, "Skipping file... Refer to error.txt");
                 }
             }
-            log("Waiting for " + (timeout*=2) + " minutes more...");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Couldn't get files from future", e);
         }
-        virtualExecutor.close();
-
-        virtualExecutor = Executors.newVirtualThreadPerTaskExecutor();
 
         return output;
     }
 
-    private void processPath(FilePredicate fp, Path p, List<File> output) throws IOException {
+    private File processPath(FilePredicate fp, Path p, List<File> output) throws IOException {
         File f = p.toFile();
-        if (f.isFile() && fp.test(f) && !output.contains(f)) {
-            output.add(f);
-        }
+        if (f.isFile() && fp.test(f) && !output.contains(f)) return f;
+        return null;
+
     }
 }
--- src/main/java/pl/magzik/Utils/LoggingInterface.java
+++ src/main/java/pl/magzik/Utils/LoggingInterface.java
@@ -11,4 +11,14 @@ public interface LoggingInterface {
     default void log(Exception ex, String msg) {
         Logger.error(ex, msg);
     }
+
+    default <E extends Exception> void logAndThrow(E ex, String msg) throws E {
+        log(ex, msg);
+        throw ex;
+    }
+
+    default <E extends RuntimeException> void logAndThrow(E ex, String msg) {
+        log(ex, msg);
+        throw ex;
+    }
 }
