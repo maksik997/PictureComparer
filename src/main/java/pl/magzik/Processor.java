b7454db6e037ec9baf98fcc712d7bd1e797f5271
Maksymilian <99613189+maksik997@users.noreply.github.com>
2024-12-04 01:26:08 +0100

    Finished New workflow - Second iteration
    -- Added JetBrains annotations for more readability.
    -- Refactored code in Processor class. Separating process method into smaller methods.

--- src/main/java/pl/magzik/Processor.java
+++ src/main/java/pl/magzik/Processor.java
@@ -1,5 +1,7 @@
 package pl.magzik;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.magzik.algorithms.Algorithm;
@@ -13,7 +15,7 @@ import java.util.stream.Collectors;
 
 /**
  * The {@code Processor} class is responsible for identifying and grouping duplicate files
- * based on a multi-step processing workflow.
+ * based on a multistep processing workflow.
  * <p>
  * The processing workflow consists of three main steps:
  * <ol>
@@ -56,7 +58,17 @@ public class Processor {
 
     private final Set<Algorithm<?>> algorithms;
 
-    public Processor(Grouper grouper, Set<Algorithm<?>> algorithms) {
+    /**
+     * Creates new Processor instance.
+     * <p>
+     *     <strong>Important!</strong><br />
+     *     When passing algorithms you should use a collection that defines some kind of ordering.
+     *     Otherwise, results may be unpredictable.
+     * </p>
+     * @param grouper A {@link Grouper} to be used in "Initial division" step.
+     * @param algorithms A {@link Collection} of {@link Algorithm} objects to be used in "Algorithms application" step.
+     * */
+    public Processor(Grouper grouper, Collection<Algorithm<?>> algorithms) {
         Objects.requireNonNull(grouper, "Grouper is null");
         Objects.requireNonNull(algorithms, "Algorithm set is null");
         if (algorithms.isEmpty() || algorithms.contains(null)) throw new NullPointerException("Algorithm set is empty or contains null.");
@@ -66,7 +78,7 @@ public class Processor {
     }
 
     /**
-     * Processes a collection of files to identify and group duplicated files based on a multi-step workflow.
+     * Processes a collection of files to identify and group duplicated files based on a multistep workflow.
      * <p>
      * This method performs the following steps:
      * <ol>
@@ -97,49 +109,123 @@ public class Processor {
      * @throws NullPointerException If the input collection or any file in it is null.
      * @throws IOException If an I/O error occur.
      */
-    public Map<File, Set<File>> process(Collection<File> files) throws IOException {
-        if (files == null || files.contains(null)) {
-            throw new NullPointerException("Input collection or its elements must not be null.");
+    @NotNull
+    @Contract("_ -> new")
+    public Map<File, Set<File>> process(@NotNull Collection<File> files) throws IOException {
+        Objects.requireNonNull(files, "Input collection must not be null");
+        if (files.contains(null)) {
+            throw new NullPointerException("Input collection must not contain null.");
         }
-
         logger.info("Processing started...");
 
         logger.info("Dividing input collection.");
-        Set<Set<File>> groupedFiles = grouper.divide(files); // This variable must contain only subsets with more than 1 element.
-        logger.info("Input collection division completed.");
+        Set<Set<File>> groupedFiles = grouper.divide(files);
+                        // This variable must contain only subsets with more than 1 element.
 
+        logger.info("Input collection division completed.");
+        groupedFiles = algorithmsApplication(groupedFiles);
+        return originalDistinction(groupedFiles);
+    }
+
+    /**
+     * Applies a series of algorithms to group files and consolidates the results by merging groups with the same key.
+     * <p>This method processes the grouped files using each algorithm in the {@code algorithms} collection. After each algorithm is applied,
+     * it consolidates the resulting maps by merging groups that have the same key and removing groups that only contain a single file.</p>
+     *
+     * @param groupedFiles a {@link Set} of {@link Set} of {@link File} instances representing the grouped files to which the algorithms should be applied.
+     *                     Each group represents a set of similar files.
+     * @return a {@link Set} of {@link Set} of {@link File} instances after applying the algorithms and consolidating the results.
+     * @throws IOException if an error occurs during algorithm application or file processing.
+     */
+    @NotNull
+    @Contract("_ -> new")
+    private Set<Set<File>> algorithmsApplication(@NotNull Set<Set<File>> groupedFiles) throws IOException {
         logger.info("Proceeding to algorithm application.");
         Map<?, Set<File>> algorithmOutput;
         for (Algorithm<?> algorithm : algorithms) {
             try {
-                logger.info("Applying algorithm: {}", algorithm.getClass().getSimpleName());
-
-                algorithmOutput = groupedFiles.parallelStream()
-                        .map(algorithm::apply)
-                        .flatMap(m -> m.entrySet().stream())
-                        .collect(Collectors.toMap(
-                                Map.Entry::getKey,
-                                Map.Entry::getValue,
-                                this::consolidate
-                        ));
-                logger.info("Algorithm applied. Eliminating unique files...");
-
-                groupedFiles = algorithmOutput.values()
-                        .stream().filter(s -> s.size() > 1)
-                        .collect(Collectors.toSet());
-
+                algorithmOutput = applyAlgorithm(algorithm, groupedFiles);
+                groupedFiles = postAlgorithmConsolidation(algorithmOutput);
                 logger.info("Step finished.");
             } catch (UncheckedIOException e) {
                 throw new IOException("Couldn't use algorithm: " + algorithm.getClass().getSimpleName() + "\nBecause: " + e.getMessage(), e.getCause());
             }
         }
+        return groupedFiles;
+    }
 
-        return groupedFiles.stream()
-            .map(s -> s.stream().toList())
+    /**
+     * Applies a single algorithm to the grouped files and returns a map of results.
+     * <p>This method processes the input groups using the specified algorithm and generates a map, where the key represents
+     * a shared characteristic, and the value is a set of files that share that characteristic.</p>
+     *
+     * @param algorithm the {@link Algorithm} to apply to the grouped files.
+     * @param groupedFiles a {@link Set} of {@link Set} of {@link File} instances representing the grouped files to be processed.
+     * @param <T> the type of the key in the generated map (e.g., perceptual hash, CRC32 checksum).
+     * @return a {@link Map} where the key is the characteristic (e.g., hash value), and the value is a set of files sharing that characteristic.
+     */
+    @NotNull
+    @Contract("_,_ -> new")
+    private <T> Map<T, Set<File>> applyAlgorithm(@NotNull Algorithm<T> algorithm, @NotNull Set<Set<File>> groupedFiles) {
+        logger.info("Applying algorithm: {}", algorithm.getClass().getSimpleName());
+        return groupedFiles.parallelStream()
+            .map(algorithm::apply)
+            .flatMap(m -> m.entrySet().stream())
             .collect(Collectors.toMap(
-                List::getFirst,
-                s -> new HashSet<>(s.subList(1, s.size()))
+                Map.Entry::getKey,
+                Map.Entry::getValue,
+                this::consolidate
             ));
+    }
+
+    /**
+     * Consolidates the results of the applied algorithm by removing groups with only one file and merging groups with identical keys.
+     * <p>This method processes the output of an algorithm, eliminating groups that contain only one file and merging groups with the same key.</p>
+     *
+     * @param algorithmOutput a {@link Map} containing the results of the algorithm, where the key is a shared characteristic (e.g., hash) and the value is a set of files.
+     * @return a {@link Set} of {@link Set} of {@link File} instances after consolidation, where only groups with more than one file are kept.
+     */
+    @NotNull
+    @Contract("_ -> new")
+    private Set<Set<File>> postAlgorithmConsolidation(@NotNull Map<?, Set<File>> algorithmOutput) {
+        logger.info("Algorithm applied. Eliminating unique files...");
+        return algorithmOutput.values()
+            .stream().filter(s -> s.size() > 1)
+            .collect(Collectors.toSet());
+    }
+
+    /**
+     * Identifies the "original" file in each group and reorganizes the groups into a map.
+     * Each group is represented as a set of files, where the first file in the group is
+     * treated as the "original". This method creates a map where:
+     * <ul>
+     *     <li>The key is the original file (the first file in each group).</li>
+     *     <li>The value is a set of files representing duplicates or similar files for that original.</li>
+     * </ul>
+     * Note that each subset of passed {@link Set} must contain more than one element.
+     *
+     * @param groupedFiles a {@link Set} of {@link Set} of {@link File} instances,
+     *                     where each inner set represents a group of files.
+     *                     The input must not be null, and each group should contain at least two files.
+     * @return a new {@link Map} where:
+     *         - The key is a {@link File} representing the original file in one group.
+     *         - The value is a {@link Set} of {@link File} instances representing duplicates or similar files.
+     *         The returned map is guaranteed not to contain null keys or values.
+     * @throws NullPointerException if {@code groupedFiles} is null, or when it contains null.
+     */
+    @NotNull
+    @Contract("_ -> new")
+    private Map<File, Set<File>> originalDistinction(@NotNull Set<Set<File>> groupedFiles) {
+        Objects.requireNonNull(groupedFiles, "groupedFiles is null.");
+        if (groupedFiles.contains(null)) throw new NullPointerException("groupedFiles contains null.");
+
+        logger.info("Identifying originals...");
+        return groupedFiles.stream()
+                .map(s -> s.stream().toList())
+                .collect(Collectors.toMap(
+                        List::getFirst,
+                        s -> new HashSet<>(s.subList(1, s.size()))
+                ));
     }
 
     /**
@@ -149,7 +235,9 @@ public class Processor {
      * @param s2 the second set to merge
      * @return a new set containing all elements from {@code s1} and {@code s2}
      */
-    private Set<File> consolidate(Set<File> s1, Set<File> s2) {
+    @NotNull
+    @Contract("_,_ -> new")
+    private Set<File> consolidate(@NotNull Set<File> s1, @NotNull Set<File> s2) {
         Set<File> out = new HashSet<>(s1);
         out.addAll(s2);
         return out;
