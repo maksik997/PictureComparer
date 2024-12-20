bbf7fbedf9d72daa8eebf9d0568c4e057fb9c7f0
Maksymilian <99613189+maksik997@users.noreply.github.com>
2024-12-20 22:50:05 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,13 @@ module PictureComparer {
     requires org.jetbrains.annotations;
     requires org.slf4j;
     requires java.desktop;
+
+    exports pl.magzik.io;
+    exports pl.magzik.algorithms;
+    exports pl.magzik.cache;
+    exports pl.magzik.predicates;
+    exports pl.magzik.grouping;
+    exports pl.magzik;
+
+    opens pl.magzik;
 }
