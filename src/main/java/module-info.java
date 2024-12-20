cf363bb90d447e514abb5ceefe09727541f3c008
Maksymilian <99613189+maksik997@users.noreply.github.com>
2024-12-20 22:33:57 +0100

    Create module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module PictureComparer {
+    requires com.github.benmanes.caffeine;
+    requires JTransforms;
+    requires org.jetbrains.annotations;
+    requires org.slf4j;
+    requires java.desktop;
+}
